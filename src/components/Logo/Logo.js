2e463153ce8b306c4727ee1163286e1085d59796
kudryasha1981 <105546279+kudryasha1981@users.noreply.github.com>
2023-12-08 19:19:39 +0300

    commit template project

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,7 +3,7 @@ import './Logo.css'
 
 function Logo () {
     return (
-        <div className='img-title'>
+        <div className='img-title-container'>
             <Link to={'/'}>
                 <img className='img-title' src='https://sun9-41.userapi.com/impg/8hixnIzXew-txDm4SiCCRguWBj5Mo1Gusynrqw/2dSUimFGxK0.jpg?size=848x329&quality=95&sign=8aafefd4042523266accdb36588959fd&type=album'></img>
             </Link>
